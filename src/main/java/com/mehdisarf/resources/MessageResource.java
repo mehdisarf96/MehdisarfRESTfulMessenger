245d677252dc707acf00ee8a0647fb2a32fd85b5
M.Mehdi <m.sfj9696@gmail.com>
2022-03-25 02:10:09 +0430

    Implement a new API for retrieving a message and access Path parameters.

--- src/main/java/com/mehdisarf/resources/MessageResource.java
+++ src/main/java/com/mehdisarf/resources/MessageResource.java
@@ -5,6 +5,7 @@ import com.mehdisarf.services.MessageService;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -19,4 +20,25 @@ public class MessageResource {
     public List<Message> getMessages() {
         return messageService.getAllMessages();
     }
+
+    @GET
+    @Path("/{messageId}")
+    @Produces(MediaType.APPLICATION_XML)
+    public Message getMessage(@PathParam("messageId") long id) {
+        return messageService.getMessage(id);
+    }
+
+    /*
+    @GET
+    @Path("/{messageId}")
+    @Produces(MediaType.APPLICATION_XML)
+    public Message getMessage(@PathParam("messageId") String id) {
+        return messageService.getMessage(Integer.parseInt(id));
+    }
+
+    // age type e method parameter e 'id' ro long bezari, jersey khodesh
+    // bahushe va conversion ro anjam mide. va dg niaz nist mese bala
+    // khodet biay dasti parse va convert ro anjam bedi.
+     */
+
 }
